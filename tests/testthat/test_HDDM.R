ec06d81ebaf556d5698703a00437f430ea06fdde
Yi-Shin Lin <yishinlin001@gmail.com>
2018-11-21 12:15:11 +0800

    BuildModel update

--- tests/testthat/test_HDDM.R
+++ tests/testthat/test_HDDM.R
@@ -15,7 +15,7 @@ test_that("HDDM", {
   
   npar <- length(GetPNames(model))
   
-  ##Population distribution
+  ## Population distribution
   pop.mean  <- c(a=2,   v.f1=4,  v.f2=3,  z=0.5, sz=0.3, sv=1,  t0=0.3)
   pop.scale <- c(a=0.5, v.f1=.5, v.f2=.5, z=0.1, sz=0.1, sv=.3, t0=0.05)
   pop.prior <- BuildPrior(
